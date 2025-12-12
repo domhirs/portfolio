1fc6bf55e8cc0656249d6e58e9cff73e9223046e
Dominik Hirsch <domi.hirsch@gmail.com>
2025-12-12 21:36:08 +0100

    Switch `styles.scss` to `styles.css` and integrate Tailwind utilities

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Dominik Hirsch');
