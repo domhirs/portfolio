4fdfab65f1dba611ecb8c32b2a50101ea9d98581
domhirs <domi.hirsch@gmail.com>
2025-12-16 19:07:54 +0100

    Update HeaderComponent to include navigation menu with `app-button` links

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../components/button/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
