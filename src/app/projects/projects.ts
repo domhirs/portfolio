1068ed65d187ebfe8154ebccc10afcfcfd83dd24
domhirs <domi.hirsch@gmail.com>
2025-12-16 19:23:29 +0100

    Make ProjectsComponent standalone by adding `standalone: true` decorator

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
+  standalone: true,
   templateUrl: './projects.html',
   styleUrls: ['./projects.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
