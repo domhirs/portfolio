316a30379b46537a437c3896b42da114511c26e7
domhirs <domi.hirsch@gmail.com>
2025-12-16 18:45:28 +0100

    Add ButtonComponent with template, styles, and unit tests

---
+++ src/app/components/button/button.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.html',
+  styleUrls: ['./button.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonComponent {
+  href = input<string>();
+  label = input<string>();
+}
