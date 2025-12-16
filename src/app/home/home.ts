114baa2a08b7b6d34d5431215c172e1ef27ab395
domhirs <domi.hirsch@gmail.com>
2025-12-16 19:08:08 +0100

    Update HomeComponent with dynamic title, optimized image, and enhanced template layout

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { ListComponent } from '../components/list/list';
+import { BreadcrumbComponent } from '../components/breadcrumb/breadcrumb';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgOptimizedImage, ListComponent, BreadcrumbComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  protected readonly title = signal('Dominik Hirsch');
 
 }
