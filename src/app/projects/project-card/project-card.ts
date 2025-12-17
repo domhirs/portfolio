27403145fdf13c040f99566ae17706f1981e75bd
domhirs <domi.hirsch@gmail.com>
2025-12-17 18:29:07 +0100

    Update ProjectCard component to fix incorrect Project import path

--- src/app/projects/project-card/project-card.ts
+++ src/app/projects/project-card/project-card.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { Project } from '../../data.service';
+import { Project } from '../../project';
 
 @Component({
   selector: 'app-project-card',
