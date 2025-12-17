d0427e760856edae5ecd7f9c11b787717fd560ad
domhirs <domi.hirsch@gmail.com>
2025-12-17 18:29:19 +0100

    Integrate ProjectCard component into ProjectsComponent and bind project data from DataService

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,10 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {DataService} from '../data.service';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {ProjectCard} from './project-card/project-card';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   templateUrl: './projects.html',
   styleUrls: ['./projects.scss'],
+  imports: [ProjectCard],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  private readonly dataService = inject(DataService);
+  readonly projects = toSignal(this.dataService.getProjects(), {initialValue: []})
+}
