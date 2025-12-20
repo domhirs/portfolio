b2b0d024323896aab25d314d932d1c65094a6ea9
domhirs <domi.hirsch@gmail.com>
2025-12-20 18:46:14 +0100

    Refactor Resume component: integrate i18n, dynamic data binding, and add `WorkExperience`/`Education` sections

--- src/app/resume/resume.ts
+++ src/app/resume/resume.ts
@@ -1,12 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { CardComponent } from '../components/card/card';
+import { Experience, Education } from '../models';
+import { TranslateModule } from '@ngx-translate/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, TranslateModule],
   templateUrl: './resume.html',
   styleUrls: ['./resume.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ResumeComponent {}
+export class ResumeComponent {
+  private readonly dataService = inject(DataService);
+
+  workExperience = signal<Experience[]>([]);
+  education = signal<Education[]>([]);
+
+  constructor() {
+    this.dataService.getExperience().subscribe(data => this.workExperience.set(data));
+    this.dataService.getEducation().subscribe(data => this.education.set(data));
+  }
+}
