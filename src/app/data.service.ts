8ad3f0a000fac3d4dd2369d4cae8e76abd8f0e34
domhirs <domi.hirsch@gmail.com>
2025-12-20 18:45:22 +0100

    Add new methods to `DataService` for fetching experience and education data

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,7 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Project} from './project';
+import {ProjectModel} from './models/project.model';
+import { Experience, Education } from './models';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,15 @@ import {Project} from './project';
 export class DataService {
   private readonly http = inject(HttpClient);
 
-  getProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>('/assets/data/projects.json');
+  getProjects(): Observable<ProjectModel[]> {
+    return this.http.get<ProjectModel[]>('/assets/data/projects.json');
+  }
+
+  getExperience(): Observable<Experience[]> {
+    return this.http.get<Experience[]>('/assets/data/experience.json');
+  }
+
+  getEducation(): Observable<Education[]> {
+    return this.http.get<Education[]>('/assets/data/education.json');
   }
 }
