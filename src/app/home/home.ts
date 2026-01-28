28f88d500b1c80b6ec21a15a96e262f601f309fe
domhirs <domi.hirsch@gmail.com>
2026-01-28 12:20:23 +0100

    Add "More Skills" section to the home page with translations

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,11 +3,12 @@ import { NgOptimizedImage } from '@angular/common';
 import { ListComponent } from '../components/list/list';
 import { BreadcrumbComponent } from '../components/breadcrumb/breadcrumb';
 import {NgIcon} from '@ng-icons/core';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage, ListComponent, BreadcrumbComponent, NgIcon],
+  imports: [NgOptimizedImage, ListComponent, BreadcrumbComponent, NgIcon, TranslatePipe],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
