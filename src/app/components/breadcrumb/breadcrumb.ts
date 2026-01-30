80925a1b65598a9cf20f0f66a849624513500068
domhirs <domi.hirsch@gmail.com>
2026-01-30 16:47:42 +0100

    Mark BreadcrumbComponent as standalone

--- src/app/components/breadcrumb/breadcrumb.ts
+++ src/app/components/breadcrumb/breadcrumb.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
-  selector: 'app-breadcrumb',
-  templateUrl: './breadcrumb.html',
-  styleUrls: ['./breadcrumb.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-breadcrumb',
+    templateUrl: './breadcrumb.html',
+    styleUrls: ['./breadcrumb.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true
 })
 export class BreadcrumbComponent {
   label = input<string>();
