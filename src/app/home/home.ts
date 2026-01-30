367714ceff3e00e5529d10a6b2757abd4ac35ff6
domhirs <domi.hirsch@gmail.com>
2026-01-30 16:52:37 +0100

    Remove redundant attributes and unused translations; simplify breadcrumb and home layout adjustments.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,17 +3,16 @@ import { NgOptimizedImage } from '@angular/common';
 import { ListComponent } from '../components/list/list';
 import { BreadcrumbComponent } from '../components/breadcrumb/breadcrumb';
 import {NgIcon} from '@ng-icons/core';
-import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage, ListComponent, BreadcrumbComponent, NgIcon, TranslatePipe],
+  imports: [NgOptimizedImage, ListComponent, BreadcrumbComponent, NgIcon],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    'class': 'block scroll-mt-16 pt-16'
+    'class': 'block scroll-mt-16'
   }
 })
 export class HomeComponent {
